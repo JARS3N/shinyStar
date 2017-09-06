f532563bd69416a2bf54c3e442f27725c1535aa9
JARS3N <JARS3N@users.noreply.github.com>
2017-09-06 12:22:44 -0400

    Create server.R

---
+++ inst/rePrintBarcodes/server.R
@@ -0,0 +1,42 @@
+library(shiny)
+#library(dplyr)
+library(PipeFish)
+library(RMySQL)
+library(openxlsx)
+#ConnectInfo<-DataStash::Triton()
+conn<-PipeFish::rmysqlCon()
+query<-paste0("SELECT DISTINCT(Lot_Num) from barcodelotview;")
+send<-dbSendQuery(conn,query)
+Lots<-dbFetch(send)
+dbDisconnect(conn)
+
+
+shinyServer(function(input, output,session) {
+  session$onSessionEnded(function() {
+    stopApp()
+  })
+
+
+  updateSelectInput(session,'Lot',choices=c("N/A",Lots$Lot_Num))
+
+  observeEvent(input$Lot,{
+    if(input$Lot!="N/A"){
+      lotn<- substr(input$Lot,2,nchar(input$Lot))
+      lotl<-substr(input$Lot,1,1)
+
+    q_str<-paste0('SELECT * from barcodelotview where Lot_Num_Input="',lotn,
+                  '" AND Cart_type="',lotl,'" ORDER BY Serial_Num;')
+    conn<-PipeFish::rmysqlCon()
+    q<-dbSendQuery(conn,q_str)
+    DF<-dbFetch(q)
+    dbDisconnect(conn)
+      dir<-"//lexnas03a-vs1-nas-s1lkq.agilent.com/LSAG/Seahorse Bioscience Chicopee/SH Consumables Labeling"
+      outfl<-'CartridgeDatabase.xlsx'
+      write.xlsx(DF,file=file.path(dir,outfl))
+    print(DF)
+    }
+  })
+
+
+
+})
