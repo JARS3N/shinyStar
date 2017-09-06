82f41a57cb81a3fb0caf904d2776fff37a2a70dd
JARS3N <JARS3N@users.noreply.github.com>
2017-09-06 12:24:49 -0400

    Add files via upload

---
+++ inst/shiny_pka/ui.R
@@ -0,0 +1,23 @@
+library(shiny)
+
+shinyUI(fluidPage(
+  titlePanel("PipeFish::pKa from asyr files"),
+  br(),
+  mainPanel(
+    textInput('pHFluor',"pH Fluorophore", value = "Fluorophore_Validation_pKa", width = NULL),
+    textInput('MFBatch',"Multi Fluor Batch", value = "NA", width = NULL),
+    selectInput("Platform", label = "Platform",
+                choices = list("96" = 1, "24" = 2, "XFp"=3),
+                selected = 1),
+    checkboxInput("CB", label = "Export from .XFD to .asyr", value = FALSE),
+    br(),
+    actionButton("BB","Run Analysis"),
+    br(),
+    actionButton("Quit", "Quit"),
+    br(),
+    textOutput("session"),
+    tableOutput("test1")
+  )
+
+)
+)
