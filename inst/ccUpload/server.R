e56986e007cb23eb26b6153494ea87f39f790def
JARS3N <JARS3N@users.noreply.github.com>
2017-09-06 11:46:53 -0400

    Create server.R

---
+++ inst/ccUpload/server.R
@@ -0,0 +1,39 @@
+library(shiny)
+library(RMySQL)
+library(asyr)
+library(XML)
+
+
+shinyServer(function(input, output,session) {
+
+  session$onSessionEnded(function() {
+    stopApp()
+  })
+
+
+  observeEvent(input$Quit, {
+    stopApp(returnValue = invisible())
+  })
+  observe({
+    output$MSG <- renderText("Ready")
+    if(input$goButton > 0){
+      output$MSG <- renderText("Select Directory")
+      DIR<-choose.dir();
+      output$MSG <- renderText("Munging Data...")
+
+      DF<- do.call('rbind',
+        lapply(
+        lapply(
+        asyr::intermediateStep(
+                  list.files(path=DIR,pattern='asyr',full.names=TRUE)
+              ),
+        asyr::process),
+        asyr::extract_wetQC
+      )
+
+      output$DF<-shiny::renderDataTable(DF)
+      output$MSG <- renderText("Communicating with Database")
+      output$MSG <- renderText("Writing to Database")
+      asyr::UploadsCC(DF)
+      output$MSG <- renderText("Complete")
+    }})
