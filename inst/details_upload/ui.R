7c799d87d54af04ae5a31c117ed933694f942665
JARS3N <JARS3N@users.noreply.github.com>
2017-09-06 12:10:30 -0400

    Add files via upload

---
+++ inst/details_upload/ui.R
@@ -0,0 +1,13 @@
+library(shiny)
+shinyUI(fluidPage(
+  h3("Machine Vision Data Upload"),
+  p('Select the directory which contains all the individual Cartridge directories'),
+  shiny::actionButton("SelectDir","Select Main Lot Directory"),
+  fluidRow(h5("Status:")),
+  fluidRow(textOutput("status")),
+  fluidRow(textOutput("status2")),
+  fluidRow(textOutput("status3")),
+  fluidRow(textOutput("status4")),
+  fluidRow(textOutput("status5"))
+)
+)
