01ae49374f34f18cc484505c5483864eba144db4
JARS3N <JARS3N@users.noreply.github.com>
2017-09-06 12:13:44 -0400

    Add files via upload

---
+++ inst/ExportAsyrToExcel/ui.R
@@ -0,0 +1,9 @@
+library(shiny)
+
+shinyUI(fluidPage(
+  p("Export Folder of asyr files to Excel"),mainPanel(
+    actionButton("EXP", "Export"),
+    br(),br(),br(),
+    actionButton("Quit", "Quit"),
+    textOutput("session"))
+))
