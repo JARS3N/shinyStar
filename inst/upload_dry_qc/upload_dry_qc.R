95ab452ea3b7dbda8c7522b439216d6481d09f1a
JARS3N <JARS3N@users.noreply.github.com>
2017-09-29 11:14:28 -0400

    Create upload_dry_qc.R

---
+++ inst/upload_dry_qc/upload_dry_qc.R
@@ -0,0 +1,43 @@
+UploadDRYQC <- function() {
+  require(shiny)
+  require(PipeFish)
+  require(miniUI)
+  library(RMySQL)
+  
+  ui<-miniPage(fluidPage(
+    p("Add QCStats from DryQC to Database"),mainPanel(
+      fileInput("file", label = "QCStats input"),
+      actionButton("EXP", "Enter"),
+      textOutput("session"),
+      actionButton("DONE", "DONE"))
+  ))
+  
+  server<-function(input,output,session) {
+    
+    stuffHappens<-  observeEvent(input$EXP,
+                                 {
+                                   IN<-fixShinyFileInput(input$file)
+                                 if(grepl("xls",IN$name)){
+                                   DF<-loadQCstats(IN$datapath)
+                                   if(nrow(DF)>1){
+                                     my_db <- rmysqlCon()
+                                     dbWriteTable(my_db, name="dryqcxf24",value=DF,
+                                                  append=TRUE,overwrite = FALSE,row.names=FALSE)
+                                     dbDisconnect(my_db)
+                                     output$session <- renderText("Script Complete,Data Uploaded")
+                                   }else{
+                                     output$session <- renderText("Error:No Data Added")
+                                   }
+                                   
+                                 }else{
+                                   output$session <- renderText("Needs to be an Excel File.")
+                                 }
+                                 
+                                 
+                                 })
+    observeEvent(input$DONE, {
+      stopApp(returnValue = invisible())
+    })
+  }
+  runGadget(ui, server,viewer = dialogViewer("UploadDRYQC", width = 400 ,height = 400))
+}
