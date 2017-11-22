727fe015ab63902b493d0da717cf5acf4a2990a9
JARS3N <JARS3N@users.noreply.github.com>
2017-11-22 13:07:20 -0500

    Update ui.R

--- inst/upload_dryqc/ui.R
+++ inst/upload_dryqc/ui.R
@@ -1,12 +1,14 @@
 
   require(shiny)
 
-  miniPage(fluidPage(
-    p("Add QCStats from DryQC to Database"),mainPanel(
+  fluidPage(
+    p("Add QCStats from DryQC to Database"),
+    mainPanel(
       fileInput("file", label = "QCStats input"),
       actionButton("EXP", "Enter"),
       textOutput("session"),
-      actionButton("DONE", "DONE"))
-  ))
+      actionButton("DONE", "DONE")
+    )
+  )
   
   
