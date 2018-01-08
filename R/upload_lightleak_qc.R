11f5726e3f325f18712411f616fc535adad430bc
JARS3N <JARS3N@users.noreply.github.com>
2018-01-08 11:54:02 -0500

    Create upload_lightleak_qc.R

---
+++ R/upload_lightleak_qc.R
@@ -0,0 +1,33 @@
+
+upload_lightleak_qc<-function(){
+
+   require(shiny)
+     require(lightleak)
+     require(xprt)
+    shinyServer(function(input, output,session) {
+      session$onSessionEnded(function() {
+        stopApp()
+      })
+      observeEvent(input$send,{
+        print("ok")
+        dir <- choose.dir()
+        if(input$CB==TRUE){xprt::asyr_to_xl(dir)}
+        if (!is.na(dir)) {
+          fls<-list.files(path=dir,full.names = T,pattern = 'xlsx')
+          lapply(
+            lapply(fls,
+                   lightleak::munge),
+            lightleak::upload
+            )
+          message("complete")
+        }else{
+          message('no directory selected')
+        }
+      }
+
+
+      )
+
+    })
+
+}
