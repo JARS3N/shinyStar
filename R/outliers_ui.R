9d4c41497ac0240e4fff11a2045bd990126e98fd
JARS3N <JARS3N@users.noreply.github.com>
2018-01-08 11:52:14 -0500

    Create outliers_ui.R

---
+++ R/outliers_ui.R
@@ -0,0 +1,23 @@
+
+outliers_ui<function(){
+require(shiny)
+
+    shinyUI(fluidPage(
+      titlePanel("Outliers"),
+      br(),
+      mainPanel(
+        textInput('expnm',"Name of Experiment", value = "OutlierAnalysis", width = NULL),
+        checkboxInput("CB", label = "Export from .Asyr", value = FALSE),
+        br(),
+        actionButton("BB","Run Analysis"),
+        br(),
+        actionButton('Quit','Quit',icon=icon('remove-sign',lib='glyphicon')),
+        br(),br(),
+        textOutput("session"),
+        tableOutput("test1")
+      )
+
+    )
+    )
+
+}
