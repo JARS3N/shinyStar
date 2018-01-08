a7d9e0ef09343ae3a840cb2298d2214fe5acc074
JARS3N <JARS3N@users.noreply.github.com>
2018-01-08 11:38:32 -0500

    Create asyr_to_xl_server.R

---
+++ R/asyr_to_xl_server.R
@@ -0,0 +1,23 @@
+
+asyr_to_xl_server<-function(){
+
+    require(shiny)
+    require(xprt)
+    shinyServer(function(input,output,session) {
+
+      session$onSessionEnded(function() {
+        stopApp()
+      })
+
+
+      observeEvent(input$Quit, {
+        stopApp(returnValue = invisible())
+      })
+      stuffHappens<-  observeEvent(input$EXP,
+                                   {fldr<-choose.dir()
+                                   xprt::asyr_to_xl(fldr)
+                                   output$session <- renderText("Script Complete")
+                                   })
+    })
+
+}
