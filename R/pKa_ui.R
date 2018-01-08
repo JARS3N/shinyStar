b48334751b44a494c40a499e7aed364d541934cb
JARS3N <JARS3N@users.noreply.github.com>
2018-01-08 12:26:54 -0500

    Update pKa_ui.R

--- R/pKa_ui.R
+++ R/pKa_ui.R
@@ -2,25 +2,27 @@ pKa_ui<-function(){
 
 require(shiny)
 
-shinyUI(fluidPage(
-  titlePanel("seastar::pKa from asyr files"),
-  br(),
-  mainPanel(
-    textInput('pHFluor',"pH Fluorophore", value = "Fluorophore_Validation_pKa", width = NULL),
-    textInput('MFBatch',"Multi Fluor Batch", value = "NA", width = NULL),
-    selectInput("Platform", label = "Platform",
-                choices = list("96" = 1, "24" = 2, "XFp"=3),
-                selected = 1),
-    checkboxInput("CB", label = "Export from .XFD to .asyr", value = FALSE),
+shinyUI(
+  fluidPage(
+    titlePanel("seastar::pKa from asyr files"),
     br(),
-    actionButton("BB","Run Analysis"),
-    br(),
-    actionButton("Quit", "Quit"),
-    br(),
-    textOutput("session"),
-    tableOutput("test1")
-  )
+    mainPanel(
+      textInput('pHFluor',"pH Fluorophore", value = "Fluorophore_Validation_pKa", width = NULL),
+      textInput('MFBatch',"Multi Fluor Batch", value = "NA", width = NULL),
+      selectInput("Platform", label = "Platform",
+                  choices = list("96" = 1, "24" = 2, "XFp"=3),
+                  selected = 1),
+      checkboxInput("CB", label = "Export from .XFD to .asyr", value = FALSE),
+      br(),
+      actionButton("BB","Run Analysis"),
+      br(),
+      actionButton("Quit", "Quit"),
+      br(),
+      textOutput("session"),
+      tableOutput("test1")
+    )
 
+  )
 )
 
 }
