29bc31931a3b7eae15745a1e9d33f205805317ff
JARS3N <JARS3N@users.noreply.github.com>
2018-01-08 11:43:28 -0500

    Create asyr_to_xl_ui.R

---
+++ R/asyr_to_xl_ui.R
@@ -0,0 +1,10 @@
+asyr_to_xl_ui<-function(){
+require(shiny)
+shinyUI(fluidPage(
+  p("Export Folder of asyr files to Excel"),mainPanel(
+    actionButton("EXP", "Export"),
+    br(),br(),br(),
+    actionButton("Quit", "Quit"),
+    textOutput("session"))
+))
+}
