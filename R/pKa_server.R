db050301c2e8ba6a0ec39ffbf19f4b8043121d90
JARS3N <JARS3N@users.noreply.github.com>
2019-08-23 12:34:52 -0400

    Update pKa_server.R

--- R/pKa_server.R
+++ R/pKa_server.R
@@ -1,28 +1,64 @@
-pKa_server<-function(){
-
-
-require(shiny)
-require(asyr)
-require(rmarkdown)
-require(xprt)
-shinyServer(function(input, output, session) {
-
-  session$onSessionEnded(function() {
-    stopApp()
+pKa_server<-function() {
+  sort_pka_data <- function(fls, nms, ph, mf) {
+    lapply(fls, asyr::new) %>%
+      lapply(., function(u) {
+        u$wetqc() %>%
+          mutate(Lot = paste0(u$type, u$lot), sn = u$sn)
+      }) %>%
+      setNames(., nms) %>%
+      dplyr::bind_rows() %>%
+      mutate(., pHbatch = ph,
+             mfbatch = mf)
+  }
+  
+  require(shiny)
+  require(asyr)
+  require(rmarkdown)
+  require(xprt)
+  library(dplyr)
+  shinyServer(function(input, output, session) {
+    session$onSessionEnded(function() {
+      stopApp()
+    })
+    
+    observeEvent(input$Quit, {
+      unlink(list.files(pattern = "temp.pdf$|Rmd$|csv$"))
+      stopApp(returnValue = invisible())
+    })
+    
+    
+    observeEvent(input$filein, {
+      unlink(list.files(pattern = "temp.pdf$|Rmd$|csv$"))
+      Dat <- sort_pka_data(input$filein$datapath,
+                           input$filein$names,
+                           input$pHFluor,
+                           input$MFBatch)
+    })
+    
+    output$BB <- downloadHandler(
+      filename = function() {
+        paste0(input$pHFluor,
+               "-",
+               input$MFBatch,
+               "-",
+               unique(Dat$Lot),
+               "_pka.pdf")
+      },
+      content = function(file) {
+        write.csv(Dat, "data.csv")
+        template <-
+          readLines(system.file("rmd/pKaTemplate.Rmd", package = "shinyStar"))
+        alt_tempalte <-
+          gsub("XLOTX",
+               input$pHFluor,
+               gsub("XBATCHX", input$MFBatch, template))
+        writeLines(text = alt_tempalte,
+                   con =  "temp.Rmd",
+                   sep = "\n")
+        rmarkdown::render("temp.Rmd", output_file = file)
+      }
+    )
+    
   })
-
-  observeEvent(input$Quit, {
-    stopApp(returnValue = invisible())
-  })
-
-  observe({
-    if(input$BB > 0 ){
-      DIR<-choose.dir()
-      if(input$CB==TRUE){xprt::xfd_to_asyr(DIR)}
-      asyr::asyr_pKa(input$pHFluor,input$MFBatch,input$Platform,DIR)
-    }
-  })
-})
-
-
 }
+
