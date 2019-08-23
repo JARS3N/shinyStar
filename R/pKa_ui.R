2869e6e07a135e5e510a15831fd9f5e0b8f5d502
JARS3N <JARS3N@users.noreply.github.com>
2019-08-23 12:35:29 -0400

    Update pKa_ui.R

--- R/pKa_ui.R
+++ R/pKa_ui.R
@@ -1,7 +1,5 @@
 pKa_ui<-function(){
-
 require(shiny)
-
 shinyUI(
   fluidPage(
     titlePanel("seastar::pKa from asyr files"),
@@ -9,20 +7,17 @@ shinyUI(
     mainPanel(
       textInput('pHFluor',"pH Fluorophore", value = "Fluorophore_Validation_pKa", width = NULL),
       textInput('MFBatch',"Multi Fluor Batch", value = "NA", width = NULL),
-      selectInput("Platform", label = "Platform",
-                  choices = list("96" = 1, "24" = 2, "XFp"=3),
-                  selected = 1),
-      checkboxInput("CB", label = "Export from .XFD to .asyr", value = FALSE),
       br(),
-      actionButton("BB","Run Analysis"),
+      shiny::fileInput("filein","upload asyr files",multiple=T),
+      #actionButton("BB","Run Analysis"),
+      shiny::downloadButton("BB","generate pdf"),
       br(),
       actionButton("Quit", "Quit"),
       br(),
       textOutput("session"),
       tableOutput("test1")
     )
-
+    
   )
 )
-
 }
