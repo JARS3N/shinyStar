fdda80106fbf9c60ea3d7455a0105cdba48a182a
JARS3N <JARS3N@users.noreply.github.com>
2019-09-03 13:32:56 -0400

    Update pKa_server.R

--- R/pKa_server.R
+++ R/pKa_server.R
@@ -1,70 +1,71 @@
 pKa_server <- function() {
+
+
+#K <- new.env()
+sort_pka_data <- function(fls, nms, ph, mf) {
+  lapply(fls, asyr::new) %>%
+    lapply(., function(u) {
+      u$wetqc() %>%
+        mutate(Lot = paste0(u$type, u$lot), sn = u$sn)
+    }) %>%
+    setNames(., nms) %>%
+    dplyr::bind_rows() %>%
+    mutate(., pHbatch = ph,
+           mfbatch = mf)
+}
+
+require(shiny)
+require(asyr)
+require(rmarkdown)
+require(xprt)
+library(dplyr)
+shinyServer(function(input, output, session) {
   K <- new.env()
-  sort_pka_data <- function(fls, nms, ph, mf) {
-    lapply(fls, asyr::new) %>%
-      lapply(., function(u) {
-        u$wetqc() %>%
-          mutate(Lot = paste0(u$type, u$lot), sn = u$sn)
-      }) %>%
-      setNames(., nms) %>%
-      dplyr::bind_rows() %>%
-      mutate(., pHbatch = ph,
-             mfbatch = mf)
-  }
-  
-  require(shiny)
-  require(asyr)
-  require(rmarkdown)
-  require(xprt)
-  library(dplyr)
-  shinyServer(function(input, output, session) {
-    session$onSessionEnded(function() {
-      stopApp()
-    })
-    
-    observeEvent(input$Quit, {
-      unlink(list.files(pattern = "temp.pdf$|Rmd$|csv$"))
-      stopApp(returnValue = invisible())
-    })
-    
-    
-    observeEvent(input$filein, {
-      K <- new.env()
-      unlink(list.files(pattern = "temp.pdf$|Rmd$|csv$"))
-      K$Dat <- sort_pka_data(input$filein$datapath,
-                             input$filein$names,
-                             input$pHFluor,
-                             input$MFBatch)
-    })
-    
-    output$BB <- downloadHandler(
-      filename = function() {
-        paste0(input$pHFluor,
-               "-",
-               input$MFBatch,
-               "-",
-               unique(K$Dat$Lot),
-               "_pka.pdf")
-      },
-      content = function(file) {
-        # cat("does Dat(a) exist?: ")
-        #print(exists('Dat'))
-        # print(head(Dat))
-        write.csv(K$Dat, "data.csv")
-        template <-
-          readLines(system.file("rmd/pKaTemplate.Rmd", package = "shinyStar"))
-        alt_tempalte <-
-          gsub("XLOTX",
-               input$pHFluor,
-               gsub("XBATCHX", input$MFBatch, template))
-        writeLines(text = alt_tempalte,
-                   con =  "temp.Rmd",
-                   sep = "\n")
-        rmarkdown::render("temp.Rmd", output_file = file)
-        # rm(K)
-      
-      }
-    )
-    
+  session$onSessionEnded(function() {
+    stopApp()
   })
+  
+  observeEvent(input$Quit, {
+    unlink(list.files(pattern = "temp.pdf$|Rmd$|csv$"))
+    stopApp(returnValue = invisible())
+  })
+  
+  
+  observeEvent(input$filein, {
+    lapply(names(K), assign, value = NULL, envir = K)
+    unlink(list.files(pattern = "temp.pdf$|Rmd$|csv$"))
+    K$Dat <- sort_pka_data(input$filein$datapath,
+                           input$filein$names,
+                           input$pHFluor,
+                           input$MFBatch)
+  })
+  
+  output$BB <- downloadHandler(
+    filename = function() {
+      paste0(input$pHFluor,
+             "-",
+             input$MFBatch,
+             "-",
+             unique(K$Dat$Lot),
+             "_pka.pdf")
+    },
+    content = function(file) {
+      write.csv(K$Dat, "data.csv")
+      template <-
+        readLines(system.file("rmd/pKaTemplate.Rmd", package = "shinyStar"))
+      alt_tempalte <-
+        gsub("XLOTX",
+             input$pHFluor,
+             gsub("XBATCHX", input$MFBatch, template))
+      writeLines(text = alt_tempalte,
+                 con =  "temp.Rmd",
+                 sep = "\n")
+      rmarkdown::render("temp.Rmd", output_file = file)
+      
+      
+    }
+  )
+  
+})
+
 }
