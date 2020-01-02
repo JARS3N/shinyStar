6c3f0525791af15424437925571def6f8df4ed42
JARS3N <JARS3N@users.noreply.github.com>
2020-01-02 13:56:36 -0500

    Update reprint_barcodes_server.R

--- R/reprint_barcodes_server.R
+++ R/reprint_barcodes_server.R
@@ -1,44 +1,39 @@
 reprint_barcodes_server<-function(){
 
-        require(shiny)
-        require(RMySQL)
-        require(openxlsx)
-        conn<- adminKraken::con_mysql()
-        query<-paste0("SELECT DISTINCT(Lot_Num) from barcodelotview;")
-        send<-dbSendQuery(conn,query)
-        Lots<-dbFetch(send)
-        dbDisconnect(conn)
+library(shiny)
+library(dplyr)
+dir<-"/mnt/LSAG/Seahorse Bioscience Chicopee/SH Consumables Labeling"
+outfl<-'CartridgeDatabase.xlsx'
+my_db <-adminKraken::con_dplyr()
+Q<-my_db %>%
+  tbl('barcodelotview') %>% 
+  select(Lot_Num) %>% 
+  distinct() %>% 
+  collect() %>% 
+  .$Lot_Num
+shinyServer(function(input, output,session) {
+  session$onSessionEnded(function() {
+    stopApp()
+  })
+  
+  
+  updateSelectInput(session,'Lot',choices=c("N/A",Q))
+  
+  observeEvent(input$Lot,{
+    if(input$Lot!="N/A"){
+      lotn<- substr(input$Lot,2,nchar(input$Lot))
+      lotl<-substr(input$Lot,1,1)
+      DF<-  my_db %>%
+        tbl('barcodelotview') %>%
+        filter(Lot_Num_Input==lotn) %>%
+        filter(Cart_type==lotl) %>%
+        arrange(.,Serial_Num) %>%
+        collect() %>% 
+        list('Matrix'=.)
 
-
-        shinyServer(function(input, output,session) {
-          session$onSessionEnded(function() {
-            stopApp()
-          })
-
-
-          updateSelectInput(session,'Lot',choices=c("N/A",Lots$Lot_Num))
-
-          observeEvent(input$Lot,{
-            if(input$Lot!="N/A"){
-              lotn<- substr(input$Lot,2,nchar(input$Lot))
-              lotl<-substr(input$Lot,1,1)
-
-            q_str<-paste0('SELECT * from barcodelotview where Lot_Num_Input="',lotn,
-                          '" AND Cart_type="',lotl,'" ORDER BY Serial_Num;')
-            conn<- adminKraken::con_mysql()
-            q<-dbSendQuery(conn,q_str)
-            DF<-dbFetch(q)
-            dbDisconnect(conn)
-              dir<-"//lexnas03a-vs1-nas-s1lkq.agilent.com/LSAG/Seahorse Bioscience Chicopee/SH Consumables Labeling"
-              outfl<-'CartridgeDatabase.xlsx'
-              write.xlsx(DF,file=file.path(dir,outfl))
-            print(DF)
-            }
-          })
-
-
-
-        })
-
-
+      library(openxlsx)
+      write.xlsx(DF,file=file.path(dir,outfl))
+      
+    }
+  })
 }
