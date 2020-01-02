dd48108ffec956ecb27e7c08ddd6f75f6157e52f
JARS3N <JARS3N@users.noreply.github.com>
2020-01-02 13:19:58 -0500

    Update reprint_bc_app.R

--- R/reprint_bc_app.R
+++ R/reprint_bc_app.R
@@ -1,3 +1,3 @@
 reprint_bc_app<-function(){
-shiny::shinyApp(shinyStar::reprint_bc_ui(),shinyStar::reprint_bc_ui())
+shiny::shinyApp(shinyStar::reprint_barcodes_ui(),shinyStar::reprint_barcodes_ui())
 }
