014e5b6fb044dd23ccf9cdcc26f31292192ff4e8
JARS3N <JARS3N@users.noreply.github.com>
2023-03-15 11:02:40 -0400

    Update pKa_ui.R

--- R/pKa_ui.R
+++ R/pKa_ui.R
@@ -8,7 +8,7 @@ shinyUI(
       textInput('pHFluor',"pH Fluorophore", value = "Fluorophore_Validation_pKa", width = NULL),
       textInput('MFBatch',"Multi Fluor Batch", value = "NA", width = NULL),
       br(),
-      shiny::fileInput("filein","upload asyr files",multiple=T),
+      shiny::fileInput("filein","upload asyr/xflr files",multiple=T),
       #actionButton("BB","Run Analysis"),
       shiny::downloadButton("BB","generate pdf"),
       br(),
